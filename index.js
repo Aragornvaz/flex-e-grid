644e5fc311e340fdc855b05a10be269852e2e147
Ascânio <ascaniovazsilva@gmail.com>
2022-12-22 15:11:19 -0300

    atualizando java

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const botaoMenu = document.querySelector('.cabecalho__menu')
-const menu = document.querySelector('menu-lateral')
+const menuLateral = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu-lateral--ativo')
+    menuLateral.classList.toggle('menu-lateral--ativo')
 })
